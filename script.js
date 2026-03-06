e945e773a8dfea8d179cbf5f5aac31a074b37805
abdul mateen <innovatecode121@gmail.com>
2026-03-06 02:41:32 -0800

    Two Task Done

---
+++ script.js
@@ -0,0 +1,15 @@
+// var bill= +prompt("Enter Your Total Bill");
+//  if (bill >1000 ){
+//     var discount= bill * 0.10;
+//     var total = bill - discount;
+//     console.log("Your Total Bill is " + total);
+//  }
+// else if (bill >=500 && bill <=1000){
+//     console.log("You Get free Delevery");
+// }
+// else {console.log("you have to buy more than 550 to get free delevery")}
+    
+var firstName = prompt("Enter Your First Name");
+var lastName = prompt("Enter Your Last Name");
+var fullName = firstName + " " + lastName;
+document.write("Welcome To Our Website " + fullName);
