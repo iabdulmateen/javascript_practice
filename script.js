ed8e42cc3a84bcec4e25f5fc956031f9c5db34b1
abdul mateen <innovatecode121@gmail.com>
2026-03-06 02:57:40 -0800

    third task done

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+// first Task
 // var bill= +prompt("Enter Your Total Bill");
-//  if (bill >1000 ){
+//  if (bill >= 2000){
+//     var discount =  bill * 0.20;
+//     var total = bill - discount;
+//     console.log("Your total bill is " + total)
+//  }
+//  else if (bill >1000 ){
 //     var discount= bill * 0.10;
 //     var total = bill - discount;
 //     console.log("Your Total Bill is " + total);
@@ -9,7 +15,30 @@
 // }
 // else {console.log("you have to buy more than 550 to get free delevery")}
     
-var firstName = prompt("Enter Your First Name");
-var lastName = prompt("Enter Your Last Name");
-var fullName = firstName + " " + lastName;
-document.write("Welcome To Our Website " + fullName);
+
+// second Task
+//  var firstName = prompt("Enter Your First Name");
+// var lastName = prompt("Enter Your Last Name");
+// var fullName = firstName + " " + lastName;
+// document.write("Welcome To Our Website " + fullName);
+
+// third Task
+var balance = 10000;
+var withdraw = +prompt("Enter The Amount You Want To Withdraw");
+if (withdraw > balance){
+    console.log("Insufficient Balance");
+}
+else if (withdraw <= balance){
+    var total = balance - withdraw;
+    console.log("Your Remaining Balance is " + total);
+}
+var balance = 10000;
+ var deposite = +prompt("enter the amount you want to deposite");
+ if (deposite > balance){
+    console.log("You cannot deposite more than your balance");
+}
+else if (deposite <= balance){
+    var total = balance + deposite;
+    console.log("Your Total Balance is " + total);
+}
+
