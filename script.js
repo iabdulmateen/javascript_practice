16dd7905fd31284be69c4e0c7341a2e13165d9da
abdul mateen <innovatecode121@gmail.com>
2026-03-13 05:17:05 -0700

    7 chapters parectice done

--- script.js
+++ script.js
@@ -23,27 +23,147 @@
 // document.write("Welcome To Our Website " + fullName);
 
 // third Task
-var balance = 10000;
-var withdraw = +prompt("Enter The Amount You Want To Withdraw");
-if (withdraw > balance){
-    console.log("Insufficient Balance");
-}
-else if (withdraw <= balance){
-    var total = balance - withdraw;
-    console.log("Your Remaining Balance is " + total);
-}
-else{
-    console.log("Invalid Input");
-}
-var balance = 10000;
- var deposite = +prompt("enter the amount you want to deposite");
- if (deposite > balance){
-    console.log("You cannot deposite more than your balance");
-}
-else if (deposite <= balance){
-    var total = balance + deposite;
-    console.log("Your Total Balance is " + total);
-}
-else{
-    console.log("Invalid Input");
-}
+
+// var matric = +prompt ("Enter Your Matric Mask");
+// var inter = +prompt("Enter Your Inter Mask");
+// if (matric < 60){
+//     document.write("You are not eligible for that");
+// }
+// else{
+//   if ( matric > 70 && inter > 50){
+//     document.write("You are eligible for that");
+// }
+// else if (matric > 60 || inter > 80){
+//     document.write("congrulation your addimision is confirmed");
+// }
+// else {
+//     document.write("you are not eligible for that");
+// }
+
+// }
+
+// var gender = prompt("Enter Your Gender");
+// var age =+prompt("Enter Your Age");
+// if (age <18 ){
+//     var withParents = prompt("Are you with parents? (yes or no)") .toLowerCase();
+//     if (withParents === "yes"){
+//         document.write("You are under 18 but you are with parents, so you can enter the park");
+//     }
+//     else{
+//         document.write("You are under 18 and you are not with parents, so you cannot enter the park");
+//     }
+// }else{
+//    if(gender === "female"){
+//     document.write("Entery Allowd welcom Ma'am");
+//    }
+//     else if (gender === "male"){
+//         var withCnic = prompt("Do you have CNIC? (yes or no)").toLowerCase();
+//         if (withCnic === "yes"){
+//             document.write("Entery Allowd welcom Sir");
+//         }
+//         else{
+//             document.write("You are  male and you do not have CNIC, so you cannot enter the park");
+//         }
+//     }else{
+//         document.write("Invalid input for gender");
+//     }
+// }
+// alert parectice
+// alert("5 * 45 = 225");
+// alert("starting");
+// alert("processing");
+// alert("ending");
+// alert("I am learning 'JavaScript' from SMIT");
+
+// alert('It's a beautiful day'); wrong because of single quote in it's
+
+// alert("It's a beautiful day");
+
+// alert('He said, "Hello"');
+
+// variable parectice
+// 1st Task
+// var favSinger = "Atif Aslam";
+// alert("My Favorite Singer is " + favSinger);    
+// favSinger = "Ali Zafer";
+// alert("Now My Favorite Singer is " + favSinger);   
+//2nd Task 
+// var coustmerMessage = ("Thanks for visiting");
+// alert(coustmerMessage);
+// 3rd Task
+// var 1stName= "Ali"; // wrong because variable name cannot start with number
+// 4th Task
+// var favQari = "Qari Abdul Basit";
+// var favQari2 = "";
+// favQari2 = favQari;
+// alert("My Favorite Qari is " + favQari2);
+  
+// varialbe for number
+// 1st Task
+// var myAge = 20;
+// var life = 5;
+// alert(myAge + life);
+//2nd Task
+// var weight = 70;
+//  weight = weight + 10;
+//  alert("My weight is " + weight);
+//  3rd Task
+// var num1 = 15;
+// var num2 = 15;
+// var sum = num1 + num2;
+// alert(sum);
+// chapter 4 parectice
+// 1st Task
+// var fullName = "Abdul mateen";
+// var favSurah = "Al-Mariyam";
+// var smitRollNo = 777837;
+
+// var @email = "test@test.com"; // wrong because variable name cannot start with @
+// oppretors
+// 1st Task
+// var num1 = 13;
+// var num2 = 2;
+// var mode = num1 % num2;
+// alert("the mode of " + num1 + " and " +  num2  + " is " + mode);
+// //  task 2
+// var complexMath = 10 + 5 * 2;
+// alert(complexMath);
+// task 3 Even Odd Logic
+// var number = +prompt("Enter a number");
+// if (number % 2 === 0) {
+//     alert(number + " is an even number");
+// }   else {
+//     alert(number + " is an odd number");
+// }
+// chapter 6 parectice
+// task 1 
+// var counter = 10;
+// counter++;
+// counter++;
+// alert(counter)
+// task 2
+// var num = 5;
+// var num2 = num++
+// alert(num);
+// alert(num2);
+// var x = 5;
+// var y = x++ + ++x;
+// alert(x);
+// alert(y);
+// var k = 10;
+// var result = k++ + ++k + k;
+// alert(result);
+// var a = 5;
+// var b = 10;
+// var c = ++a + b++ - a++ + --b;
+// alert(c);
+// chapter 7 parectice
+// task 1
+var a = 2;
+var b = 3;
+var check = (a++ + ++b) - (b-- + ++a);
+alert(check);
+
+
+
+
