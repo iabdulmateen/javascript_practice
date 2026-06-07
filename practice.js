5c96f69263a992c59822852f200c08e6c47b86d3
abdul mateen <innovatecode121@gmail.com>
2026-06-07 09:01:42 +0500

    index
    again javascript practice start

---
+++ practice.js
@@ -0,0 +1,30 @@
+// alert("Karachi");
+// var myName = "Abdul Mateen";
+// alert(myName);
+// var quantity = 20;
+// var total = quantity + 5;
+// alert(total);
+// var nameQuestion = "what is your namr?";
+// var name = prompt(nameQuestion);
+// alert(name);
+// var city = "karachi";
+// if (city === "karachi") {
+//   alert("zaberdast!");
+// }
+// var price = +prompt("Enter Item Price:");
+// var discount = price * 0.2;
+// var finalPrice = price - discount;
+// if (finalPrice > 200) {
+//   alert("20% Discount applied! Total bill is: " + " " + finalPrice);
+// } else {
+//   alert("sorry you are not eligible ");
+// }
+var light = "what is the light color?";
+var trafficLight = prompt(light);
+if (trafficLight.toLowerCase() === "red") {
+  alert("Stop!");
+} else if (trafficLight.toLowerCase() === "yellow") {
+  alert("Ready!");
+} else {
+  alert("Go!");
+}
